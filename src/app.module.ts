403dd70700071d8e89e425729d35217a2e724c58
Alfredo <21030761@itcelaya.edu.mx>
2024-01-14 12:35:26 -0600

    Set of Docker yml and update of README

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+// Modules of the APP
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { SeedModule } from './seed/seed.module';
+// Module for serve static file
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+// Module for DB
+import { MongooseModule } from '@nestjs/mongoose';
+
+// B.D's relacional: product, user, order, sale, sale_detail, transaccion, 
+// B.D's no relacional: 
 
 @Module({
-  imports: [ProductModule, UserModule, SeedModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    // MongoDriver
+    MongooseModule.forRoot('mongodb://localhost:27017/ecommerce'),
+    // Api modules
+    ProductModule, UserModule, SeedModule
+  ],
   controllers: [],
   providers: [],
 })
